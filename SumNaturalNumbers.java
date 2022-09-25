c2d2d48578778c406c2983bab71fb3f3f619330f
Johnjinson <64381165+Johnjinson@users.noreply.github.com>
2022-09-25 12:28:04 +0530

    Create SumNaturalNumbers.java
    Program to add sum of all natural number from 1 to 50.

---
+++ SumNaturalNumbers.java
@@ -0,0 +1,12 @@
+class SumNaturalNumbers {
+    public static void main(String[] args) {
+        
+        int n, sum;
+        n = 50;
+        
+        sum = (n * (n + 1)) / 2;    // Formula for 1 to n natural numbers
+        
+        System.out.println("\nSum of 1 to 50 natural number is : " + sum);
+        
+    }
+}
